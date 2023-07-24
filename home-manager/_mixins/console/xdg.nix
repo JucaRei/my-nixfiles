55b2de3b2f0be2c014098308425eb6fd8bf044d5
Reinaldo <reinaldo800@gmail.com>
2023-07-24 14:57:18 -0300

    fix

--- home-manager/_mixins/console/xdg.nix
+++ home-manager/_mixins/console/xdg.nix
@@ -76,7 +76,6 @@ in
         #XDG_CONTAINERS_DIR = "${config.home.homeDirectory}/containers-data";
         XDG_WORKSPACE_DIR = "${config.home.homeDirectory}/Documents/workspace";
         XDG_CACHE_HOME = "${config.home.homeDirectory}/.cache";
-        XDG_CONFIG_HOME = lib.mkDefault "${config.home.homeDirectory}./config";
         XDG_DATA_HOME = "${config.home.homeDirectory}/.local/share";
         XDG_BIN_HOME = "${config.home.homeDirectory}/.local/bin";
       };
