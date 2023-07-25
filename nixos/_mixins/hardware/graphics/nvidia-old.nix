736da7728efa7fc2a9d0d20cb05ff7aae04b514e
Reinaldo <reinaldo800@gmail.com>
2023-07-24 23:41:43 -0300

    tesitn

--- nixos/_mixins/hardware/graphics/nvidia-old.nix
+++ nixos/_mixins/hardware/graphics/nvidia-old.nix
@@ -1,13 +1,14 @@
 { config, ... }: {
   hardware = {
     nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.legacy_340;
       # Enable the nvidia settings menu
       nvidiaSettings = true;
       forceFullCompositionPipeline = true;
       powerManagement.enable = true;
     };
   };
+
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_340;
   services.xserver.videoDrivers = [ "nvidia " ];
 
   boot.blacklistedKernelModules = [ "nouveau" ];
