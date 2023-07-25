67b96c76dfea7766ad2b6870104f77d33353e471
Reinaldo <reinaldo800@gmail.com>
2023-07-25 02:45:09 -0300

    fix

--- scripts/bios.sh
+++ scripts/bios.sh
@@ -15,8 +15,24 @@
 #quit
 #END
 
+#parted /dev/sda -- mklabel msdos
+## parted /dev/sda -- mkpart no-fs 1MB 
+#parted /dev/sda -- mkpart primary ext2 1 2
+#parted /dev/sda -- set 1 boot on
+#parted /dev/sda -- mkpart primary xfs 2 100%
+#parted /dev/sda -- name 2 'NIXOS'
+#parted print
+#parted /dev/sda -- mkpart primary 1MB 100%
+#parted /dev/sda -- set 1 bios_grub on
+
 parted /dev/sda -- mklabel msdos
-# parted /dev/sda -- mkpart no-fs 1MB 
+#parted /dev/sda -- mkpart primary 1MB -8GB # Swap
 parted /dev/sda -- mkpart primary 1MB 100%
 parted /dev/sda -- set 1 boot on
-#parted /dev/sda -- set 1 bios_grub on
+
+DEVICE=/dev/sda
+opts="defaults,noatime,nodiratime"
+
+#mkfs.xfs -f -L "NIXOS" ${DEVICE}2
+mkfs.xfs -f -L "NIXOS" /dev/sda
+mount -o $opts /dev/disk/by-label/NIXOS /mnt/
--- scripts/xfs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-DEVICE=/dev/sda
-opts="defaults,noatime,nodiratime"
-
-#mkfs.xfs -f -L "NIXOS" ${DEVICE}2
-mkfs.xfs -f -L "NIXOS" ${DEVICE}1
-mount -o $opts /dev/disk/by-partlabel/NIXOS /mnt
