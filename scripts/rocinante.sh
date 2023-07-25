9fb95142f87730d6328c12b8ce112e858bfb4b53
Reinaldo <reinaldo800@gmail.com>
2023-07-24 22:44:12 -0300

    fix

--- scripts/rocinante.sh
+++ scripts/rocinante.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 export drive="/dev/sda"
 
-#        mkpart no-fs 0 1024KiB \
+#        
 
 sgdisk -Z $drive
 parted -s -a optimal $drive \
        mklabel gpt \
-       mkpart no-fs 0 1MiB \
+       mkpart no-fs 0 1024KiB \
        align-check optimal 1 \
        set 1 bios_grub on \
        print
 
 
-parted --script $drive -- \
-        mklabel gpt \
-        mkpart no-fs 0 1MiB \
-        set 1 bios_grub on \
-        align-check optimal 1 \
-        print
-
+#parted --script $drive -- \
+#        mklabel gpt \
+#        mkpart no-fs 0 1MiB \
+#        set 1 bios_grub on \
+#        align-check optimal 1 \
+#        print
+#
 mkfs.xfs $drive -f -L "NIXOS"
 
 opts="defaults,noatime,nodiratime"
