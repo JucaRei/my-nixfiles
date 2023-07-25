98eb1766581b03f0b8f54bb28a2eeca7d969823c
Reinaldo <reinaldo800@gmail.com>
2023-07-25 03:59:28 -0300

    fix

--- nixos/rocinante/default.nix
+++ nixos/rocinante/default.nix
@@ -77,7 +77,6 @@
     inxi
   ];
 
-  services.xserver.videoDrivers = [ "nvidia " ];
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
