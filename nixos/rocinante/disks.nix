d7e8cb837a875714dd873cbf357cb844112d06f9
Reinaldo <reinaldo800@gmail.com>
2023-07-27 12:45:21 -0300

    testing

--- nixos/rocinante/disks.nix
+++ nixos/rocinante/disks.nix
@@ -5,61 +5,64 @@ let
   defaultExt4Opts = [ "defaults" "noatime" "nodiratime" "commit=60" "barrier=0 " ];
 in
 {
+  disko.devices = {
+    disk = {
+      sda = {
+        #device = "/dev/sda";
+        device = builtins.elemAt disks 0;
+        type = "disk";
+        content = {
+          type = "gpt";
+          #type = "msdos";
+          partitions = [{
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              flags = [ "bios_grub" ];
+            }
+              {
+                name = "NIXOS";
+                start = "1M";
+                end = "100%";
+                content = {
+                  type = "filesystem";
+                  # Overwirte the existing filesystem
+                  #extraArgs = [ "-f" ];
+                  format = "ext4";
+                  mountpoint = "/";
+                  mountOptions = defaultExt4Opts;
+                };
+              };
+          }];
+        };
+      };
+    };
+  };
+
   #disko.devices = {
   #  disk = {
   #    sda = {
   #      device = "/dev/sda";
   #      type = "disk";
   #      content = {
-  #        #type = "gpt";
-  #        type = "msdos";
-  #        partitions = {
-  #          boot = {
-  #            size = "1M";
-  #            type = "EF02"; # for grub MBR
-  #            #flags = [ "bios_grub" ];
-  #          };
-  #          nixos = {
-  #            size = "100%";
+  #        type = "table";
+  #        format = "msdos";
+  #        partitions = [
+  #          {
+  #            name = "NIXOS";
+  #            part-type = "primary";
+  #            start = "1M";
+  #            end = "100%";
+  #            bootable = true;
   #            content = {
   #              type = "filesystem";
   #              format = "ext4";
-  #              # Overwirte the existing filesystem
-  #              #extraArgs = [ "-f" ];
   #              mountpoint = "/";
-  #              mountOptions = defaultExt4Opts;
   #            };
-  #          };
-  #        };
+  #          }
+  #        ];
   #      };
   #    };
   #  };
   #};
-
-  disko.devices = {
-    disk = {
-      sda = {
-        device = "/dev/sda";
-        type = "disk";
-        content = {
-          type = "table";
-          format = "msdos";
-          partitions = [
-            {
-              name = "NIXOS";
-              part-type = "primary";
-              start = "1M";
-              end = "100%";
-              bootable = true;
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            }
-          ];
-        };
-      };
-    };
-  };
 }
--- scripts/bios.sh
+++ scripts/bios.sh
@@ -15,24 +15,41 @@
 #quit
 #END
 
+#############
+### MSDOS ###
+#############
+
 #parted /dev/sda -- mklabel msdos
 ## parted /dev/sda -- mkpart no-fs 1MB 
 #parted /dev/sda -- mkpart primary ext2 1 2
 #parted /dev/sda -- set 1 boot on
 #parted /dev/sda -- mkpart primary xfs 2 100%
 #parted /dev/sda -- name 2 'NIXOS'
-#parted print
-#parted /dev/sda -- mkpart primary 1MB 100%
-#parted /dev/sda -- set 1 bios_grub on
+#parted /dev/sda -- print
+##parted /dev/sda -- mkpart primary 1MB 100%
+##parted /dev/sda -- set 1 bios_grub on
 
-parted /dev/sda -- mklabel msdos
-#parted /dev/sda -- mkpart primary 1MB -8GB # Swap
-parted /dev/sda -- mkpart primary 1MB 100%
-parted /dev/sda -- set 1 boot on
 
-DEVICE=/dev/sda
-OPTS="defaults,noatime,nodiratime"
+###############################################
+##parted /dev/sda -- mklabel msdos
+###parted /dev/sda -- mkpart primary 1MB -8GB # Swap
+##parted /dev/sda -- mkpart primary 1MB 100%
+##parted /dev/sda -- set 1 boot on
+##
+##DEVICE=/dev/sda
+##OPTS="defaults,noatime,nodiratime"
+##
+###mkfs.xfs -f -L "NIXOS" ${DEVICE}2
+##mkfs.xfs -f -L "NIXOS" /dev/sda1
+##mount -o ${OPTS} /dev/disk/by-label/NIXOS /mnt
 
-#mkfs.xfs -f -L "NIXOS" ${DEVICE}2
-mkfs.xfs -f -L "NIXOS" /dev/sda1
-mount -o ${OPTS} /dev/disk/by-label/NIXOS /mnt
+################
+### GPT bios ###
+################
+
+parted /dev/sda -- mklabel gpt
+parted /dev/sda -- mkpart primary ext2 1 2
+parted /dev/sda -- set 1 bios_grub on
+parted /dev/sda -- mkpart primary xfs 2 100%
+parted /dev/sda -- name 2 'NIXOS'
+parted /dev/sda -- print
