9cb365a42cb6a37af05b98d3b3555328aeb75f88
Reinaldo <reinaldo800@gmail.com>
2023-07-26 21:52:30 -0300

    test

--- home-manager/_mixins/console/zsh.nix
+++ home-manager/_mixins/console/zsh.nix
@@ -1,17 +1,20 @@
 { pkgs, inputs, config, ... }:
-let
-  prompt = pkgs.writeShellScriptBin "prompt" ''${builtins.readFile ../../../assets/zsh/prompt.zsh }'';
-  kubectl = pkgs.writeShellScriptBin "kubectl" ''${builtins.readFile ../../../assets/zsh/kubectl.zsh }'';
-in
+#let
+#  prompt = pkgs.writeShellScriptBin "prompt" ''${builtins.readFile ../../../assets/zsh/prompt.zsh }'';
+#  kubectl = pkgs.writeShellScriptBin "kubectl" ''${builtins.readFile ../../../assets/zsh/kubectl.zsh }'';
+#in
 {
-  home.packages = with pkgs; [
-    prompt
-    kubectl
-  ];
-  programs = {
-    zsh = {
-      enable = true;
-      enableAutosuggestions = true;
+  home = {
+    packages = [ pkgs.zsh ];
+    file = {
+      home.file."prompt.zsh".source = ../../../assets/zsh/prompt.zsh;
+      home.file."kubeclt.zsh".source = ../../../assets/zsh/prompt.zsh;
+    };
+    programs = {
+      zsh = {
+        enable = true;
+        enableAutosuggestions = true;
+      };
     };
   };
 }
