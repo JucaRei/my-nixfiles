1dc00ba4cf4de1069adff1842c6d745efc03a2a2
Reinaldo <reinaldo800@gmail.com>
2023-07-27 14:19:11 -0300

    msdos

--- nixos/rocinante/disks.nix
+++ nixos/rocinante/disks.nix
@@ -7,38 +7,38 @@ in
 {
   #device = "/dev/sda";
   #type = "msdos";
-  disko.devices = {
-    disk = {
-      vda = {
-        type = "disk";
-        device = builtins.elemAt disks 0;
-        content = {
-          type = "table";
-          format = "gpt";
-          partitions = [{
-            name = "primary";
-            start = "0%";
-            end = "1M";
-            #type = "EF02"; # for grub MBR
-            flags = [ "bios_grub" ];
-          }
-            {
-              name = "NIXOS";
-              start = "1M";
-              end = "100%";
-              content = {
-                type = "filesystem";
-                # Overwirte the existing filesystem
-                extraArgs = [ "-f" ];
-                format = "ext4";
-                mountpoint = "/";
-                mountOptions = defaultExt4Opts;
-              };
-            }];
-        };
-      };
-    };
-  };
+  #disko.devices = {
+  #  disk = {
+  #    vda = {
+  #      type = "disk";
+  #      device = builtins.elemAt disks 0;
+  #      content = {
+  #        type = "table";
+  #        format = "gpt";
+  #        partitions = [{
+  #          name = "primary";
+  #          start = "0%";
+  #          end = "1M";
+  #          #type = "EF02"; # for grub MBR
+  #          flags = [ "bios_grub" ];
+  #        }
+  #          {
+  #            name = "NIXOS";
+  #            start = "1M";
+  #            end = "100%";
+  #            content = {
+  #              type = "filesystem";
+  #              # Overwirte the existing filesystem
+  #              #extraArgs = [ "-f" ];
+  #              format = "ext4";
+  #              mountpoint = "/";
+  #              mountOptions = defaultExt4Opts;
+  #            };
+  #          }];
+  #      };
+  #    };
+  #  };
+  #};
 
   #disko.devices = {
   #  disk = {
@@ -66,4 +66,30 @@ in
   #    };
   #  };
   #};
+
+  disk = {
+    sda = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "table";
+        format = "msdos";
+        partitions = [
+          {
+            name = "NIXOS";
+            part-type = "primary";
+            start = "1M";
+            end = "100%";
+            bootable = true;
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = defaultExt4Opts;
+            };
+          }
+        ];
+      };
+    };
+  };
 }
