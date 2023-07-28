8eda4e08cbaa16bc621fe68a6960aaf68322cbbd
Reinaldo <reinaldo800@gmail.com>
2023-07-28 17:45:33 -0300

    btop themes

--- home-manager/_mixins/console/btop.nix
+++ home-manager/_mixins/console/btop.nix
@@ -216,5 +216,5 @@ _: {
   };
 
   xdg.configFile."btop/themes/cyberpunk_neon.theme".source =
-    ../../../assets/btop/cyberpunk_neon.theme;
+    ../../../assets/btop/themes/cyberpunk_neon.theme;
 }
