ee1f65ad7c88583c889425096c235d2b22f98325
Reinaldo <reinaldo800@gmail.com>
2023-08-14 15:24:41 -0300

    teste

--- nixos/vm/disks.nix
+++ nixos/vm/disks.nix
@@ -57,10 +57,10 @@ in
                     mountOptions = [ "subvol=@home" "rw" "noatime" "nodiratime" "ssd" "nodatacow" "compress-force=zstd:5" "space_cache=v2" "commit=120" "discard=async" ];
                     mountpoint = "/home";
                   };
-                  "/.snapshots" = {
-                    mountOptions = [ "subvol=@snapshots" "rw" "noatime" "nodiratime" "ssd" "nodatacow" "compress-force=zstd:5" "space_cache=v2" "commit=120" "discard=async" ];
-                    mountpoint = "/.snapshots";
-                  };
+                  #"/.snapshots" = {
+                  #  mountOptions = [ "subvol=@snapshots" "rw" "noatime" "nodiratime" "ssd" "nodatacow" "compress-force=zstd:5" "space_cache=v2" "commit=120" "discard=async" ];
+                  #  mountpoint = "/.snapshots";
+                  #};
                   "/tmp" = {
                     mountOptions = [ "subvol=@tmp" "rw" "noatime" "nodiratime" "ssd" "nodatacow" "compress-force=zstd:5" "space_cache=v2" "commit=120" "discard=async" ];
                     mountpoint = "/tmp";
