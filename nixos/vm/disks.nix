5d23bdd14b27cc83c13e14bc5d8bf2c510f381b9
Reinaldo <reinaldo800@gmail.com>
2023-08-14 15:28:48 -0300

    fix

--- nixos/vm/disks.nix
+++ nixos/vm/disks.nix
@@ -15,7 +15,6 @@ in
           partitions = [
             {
               name = "ESP";
-              priority = 1;
               start = "0%";
               end = "550MiB";
               bootable = true;
@@ -57,10 +56,10 @@ in
                     mountOptions = [ "subvol=@home" "rw" "noatime" "nodiratime" "ssd" "nodatacow" "compress-force=zstd:5" "space_cache=v2" "commit=120" "discard=async" ];
                     mountpoint = "/home";
                   };
-                  #"/.snapshots" = {
-                  #  mountOptions = [ "subvol=@snapshots" "rw" "noatime" "nodiratime" "ssd" "nodatacow" "compress-force=zstd:5" "space_cache=v2" "commit=120" "discard=async" ];
-                  #  mountpoint = "/.snapshots";
-                  #};
+                  "/.snapshots" = {
+                   mountOptions = [ "subvol=@snapshots" "rw" "noatime" "nodiratime" "ssd" "nodatacow" "compress-force=zstd:5" "space_cache=v2" "commit=120" "discard=async" ];
+                   mountpoint = "/.snapshots";
+                  };
                   "/tmp" = {
                     mountOptions = [ "subvol=@tmp" "rw" "noatime" "nodiratime" "ssd" "nodatacow" "compress-force=zstd:5" "space_cache=v2" "commit=120" "discard=async" ];
                     mountpoint = "/tmp";
