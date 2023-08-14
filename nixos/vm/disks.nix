a5bbf5d15b6017c144ff1f81c0421da004936453
Reinaldo <reinaldo800@gmail.com>
2023-08-14 10:42:29 -0300

    testing

--- nixos/vm/disks.nix
+++ nixos/vm/disks.nix
@@ -1,68 +1,69 @@
-{ disks ? [ "/dev/vda" ], ... }:
+_:
 let
   # "subvol=@"
   options = [ "rw" "noatime" "nodiratime" "ssd" "nodatacow" "compress-force=zstd:5" "space_cache=v2" "commit=120" "discard=async" ];
 in
 {
-
-  disk = {
-    vda = {
-      device = builtins.elemAt disks 0;
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            priority = 1;
-            name = "ESP";
-            start = 0;
-            end = "512MiB";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "defaults" "noatime" "nodiratime" ];
+  disko.devices = {
+    disk = {
+      vda = {
+        type = "disk";
+        device = /dev/disk/by-diskseq/1;
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              #priority = 1;
+              name = "ESP";
+              start = 0;
+              end = "512MiB";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "defaults" "noatime" "nodiratime" ];
+              };
             };
-          };
-          swap = {
-            start = "512MiB";
-            size = "6GiB";
-            content = {
-              type = "swap";
-              randomEncryption = true;
-              resumeDevice = true;
+            swap = {
+              start = "512MiB";
+              size = "6GiB";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+                resumeDevice = true;
+              };
             };
-          };
-          root = {
-            size = "100%";
-            content = {
-              type = "btrfs";
-              extraArgs = [ "-f" ]; # Override existing partition
-              # Subvolumes must set a mountpoint in order to be mounted,
-              # unless their parent is mounted
-              subvolumes = {
-                # Subvolume name is different from mountpoint
-                "/rootfs" = {
-                  mountpoint = "/";
-                  mountOptions = [ "subvol=@" options ];
-                };
-                # Subvolume name is the same as the mountpoint
-                "/home" = {
-                  mountOptions = [ "subvol=@home" options ];
-                  mountpoint = "/home";
-                };
-                "/.snapshots" = {
-                  mountOptions = [ "subvol=@snapshots" options ];
-                  mountpoint = "/.snapshots";
-                };
-                "/tmp" = {
-                  mountOptions = [ "subvol=@tmp" options ];
-                  mountpoint = "/tmp";
-                };
-                "/nix" = {
-                  mountOptions = [ "subvol=@nix" options ];
-                  mountpoint = "/nix";
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+                subvolumes = {
+                  # Subvolume name is different from mountpoint
+                  "/rootfs" = {
+                    mountpoint = "/";
+                    mountOptions = [ "subvol=@" options ];
+                  };
+                  # Subvolume name is the same as the mountpoint
+                  "/home" = {
+                    mountOptions = [ "subvol=@home" options ];
+                    mountpoint = "/home";
+                  };
+                  "/.snapshots" = {
+                    mountOptions = [ "subvol=@snapshots" options ];
+                    mountpoint = "/.snapshots";
+                  };
+                  "/tmp" = {
+                    mountOptions = [ "subvol=@tmp" options ];
+                    mountpoint = "/tmp";
+                  };
+                  "/nix" = {
+                    mountOptions = [ "subvol=@nix" options ];
+                    mountpoint = "/nix";
+                  };
                 };
               };
             };
